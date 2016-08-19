21149448f1b0c15238bd1968650891eba13700a6
oakinyelure <oakinyelure@hotmail.com>
2016-08-19 02:02:40 -0400

    Update submitData.js

--- getEmailNotification/src/main/resources/public/submitData.js
+++ getEmailNotification/src/main/resources/public/submitData.js
@@ -2,12 +2,12 @@
 //Olusegun Akinyelure.
 $(function(){
 
-    $('#getNotified').submit(function(e){
+    $("#getNotified").submit(function(e){
             //input values stored in json format.
             var dat = {
-                    fullName : $('#fullName').val(),
-                    email : $('#email').val()
-                }
+                    fullName : $("#fullName").val(),
+                    email : $("#email").val()
+                };
              //end of data collection
 
            //Ajax to server data transfer took place here
@@ -15,22 +15,22 @@ $(function(){
             datatype : "json",
             contentType : "application/json; charset=utf-8",
             type : "POST",
-            url :'/save',
+            url :"/save",
             data: JSON.stringify(dat),
             success: function(data){
-                $('#successful').addClass("alert alert-success");
-                $('#checkMark').addClass("glyphicon glyphicon-ok");
-                $('#successful').html("Your email has been Submitted. You will notified as soon as we come live");
-                $('#fullName').val("");
-                $('#email').val("");
+                $("#successful").addClass("alert alert-success");
+                $("#checkMark").addClass("glyphicon glyphicon-ok");
+                $("#successful").html("Your email has been Submitted. You will notified as soon as we come live");
+                $("#fullName").val("");
+                $("#email").val("");
                 setTimeout(function() {
-                					$('#successful').fadeOut("slow");}, 2000 );
+                					$("#successful").fadeOut("slow");}, 2000 );
             },
 
             error: function(){
-                $('#error').addClass("alert alert-danger");
-                 $('#exclamationSign').addClass("glyphicon glyphicon-exclamation-sign");
-                 $('#error').html("There was problem submitting your email. Please try Again Later");
+                $("#error").addClass("alert alert-danger");
+                 $("#exclamationSign").addClass("glyphicon glyphicon-exclamation-sign");
+                 $("#error").html("There was problem submitting your email. Please try Again Later");
                              setTimeout(function() {
                                $('#error').fadeOut("slow");}, 2000 );
             },
