2d2d47bd7d9c6f522a13bcd40e738679173fec7e
ifeify <ifeify92@gmail.com>
2016-08-20 18:02:11 -0500

    changed default port. Port number can now also be passed in as a JVM arg

--- src/main/java/com/cti/Routes.java
+++ src/main/java/com/cti/Routes.java
@@ -1,6 +1,5 @@
-/**
- * Created by Olusegun on 8/8/2016.
- */
+package com.cti;
+
 import com.cti.connectin.DbConnection;
 import com.cti.domain.EmailObjects;
 import freemarker.template.Configuration;
@@ -9,12 +8,21 @@ import org.slf4j.LoggerFactory;
 import static com.cti.util.JsonUtil.*;
 import static spark.Spark.*;
 
-
+/**
+ * Created by Olusegun on 8/8/2016.
+ */
 public class Routes {
     private static final Logger logger = LoggerFactory.getLogger(Routes.class);
 
     public static DbConnection connectMongo = new DbConnection();
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        String portNumber = System.getProperty("PORT");
+        // default port is 8080. Otherwise, use user supplied port
+        // number if passed in as an argument
+        port(8080);
+        if(portNumber != null) {
+            port(Integer.parseInt(portNumber));
+        }
         staticFileLocation("/public");
         Configuration loaderConfig = new Configuration();
         loaderConfig.setClassForTemplateLoading(Routes.class, "/public");
